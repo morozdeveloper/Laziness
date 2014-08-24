e3d1e755575594de4807dc77f39fef99c841cfd6
morozdeveloper <morozdeveloper@gmail.com>
2014-08-24 15:41:15 +0400

    update

--- app/src/main/java/com/example/user/laziness/MyActivity.java
+++ app/src/main/java/com/example/user/laziness/MyActivity.java
@@ -4,16 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+
+    TextView out;
+    Button btnyes;
+    Button btnno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
 
+        out = (TextView) findViewById(R.id.out);
+        btnyes = (Button) findViewById(R.id.btnyes);
+        btnno = (Button) findViewById(R.id.btnno);
+
+        out.setText("Ты лентяй?");
+        btnyes.setText("Да");
+        btnno.setText("Нет");
+
+
+        View.OnClickListener oclbtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()){
+                    case R.id.btnyes:
+                        out.setText("Не грусти, человек! Все мы ленивы...");
+                        break;
+                    case R.id.btnno:
+                        out.setText("Хехе Лжешь ведь, лжешь..");
+                        break;
+
+                }
+            }
+        };
+
+        btnyes.setOnClickListener(oclbtn);
+        btnno.setOnClickListener(oclbtn);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,3 +68,4 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
